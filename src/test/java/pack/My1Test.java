f1f2e15aacdaa6404e01de0f7f244401c2d0814e
andrii _kolomakin <andrii.kolomakin@microfocus.com>
2022-06-12 18:00:26 +0200

    s

--- src/test/java/pack/My1Test.java
+++ src/test/java/pack/My1Test.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\MyTest_OctaneGherkinResults.xml",
-                features="src/test/resources/DE bdd 2_24002.feature")
-public class MyTest2 {
+                features="src/test/resources/DE bdd 1_23005.feature")
+public class My1Test {
 }
