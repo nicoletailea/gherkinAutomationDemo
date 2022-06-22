79918e1f80f0de2ec8effc410db76e8e64c718a7
andrii _kolomakin <andrii.kolomakin@microfocus.com>
2022-06-22 12:20:18 +0200

    ser

--- src/test/java/pack/MyStepdefs.java
+++ src/test/java/pack/MyStepdefs.java
@@ -1,10 +1,8 @@
 package pack;
 
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import jdk.jshell.spi.ExecutionControl;
 import org.junit.Assert;
 
 public class MyStepdefs {
