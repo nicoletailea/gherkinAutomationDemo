c92d4d61ce0ee3be7173f3a58228f679ac029a81
nicoletailea <45171859+nicoletailea@users.noreply.github.com>
2022-08-02 17:30:47 +0300

    Update MyStepdefs.java

--- src/test/java/pack/MyStepdefs.java
+++ src/test/java/pack/MyStepdefs.java
@@ -7,17 +7,17 @@ import cucumber.api.java.en.When;
 import org.junit.Assert;
 
 public class MyStepdefs {
-    @Given("given")
+    @Given("sdsd")
     public void given() {
         System.out.println("Given");
     }
 
-    @When("when")
+    @When("ds")
     public void when() {
         System.out.println("When");
     }
 
-    @Then("then")
+    @Then("sdfsdf")
     public void then() {
         System.out.println("Then");
     }
